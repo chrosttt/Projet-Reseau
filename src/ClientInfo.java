54a92dbfabb97ba664ebe3933c8d080c3048ae01
Zaki <mirola-medhat.zaki@etu.univ-lyon1.fr>
2026-03-30 11:58:04 +0200

    création de classe ClientInfo

--- src/ClientInfo.java
+++ src/ClientInfo.java
@@ -1,1 +1,28 @@
 //encapsule les informations d’un client (pseudo, adresse IP, port)
+import java.net.InetAddress;
+import java.net.DatagramSocket;
+public class ClientInfo {
+    private String pseudo;
+    private InetAddress adresseIP;
+    private int port;
+    private DatagramSocket socket;
+
+    public ClientInfo(String pseudo, InetAddress adresseIP, int port){
+        this.port = port;
+        this.adresseIP = adresseIP;
+        this.pseudo = pseudo;
+
+    }
+    public String getPseudo (){
+        return pseudo;
+    }
+    public InetAddress getAdresseIP(){
+        return adresseIP;
+    }
+    public int getPort (){
+        return port;
+    }
+    public DatagramSocket getSocket() {
+        return socket;
+    }
+}
